d425104c2e03b6f981e5f864b9aff7876b3546f5
king-handy <a.king.life@pm.me>
2021-07-22 19:52:31 -0400

    router dom

--- use-my-tech-app/src/App.js
+++ use-my-tech-app/src/App.js
@@ -1,13 +1,12 @@
 
 import './App.css';
-import LoginPage from './LoginPage'
+import React from 'react';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-      <LoginPage />
-      </header>
+    <div>
       
     </div>
   );
---
+++ use-my-tech-app/src/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div class='navBar'>
+
+        </div>
+    )
+}
+
+export default Nav;
