558c569c1577b9ea16821f30d1e6eacc4b40293d
king-handy <a.king.life@pm.me>
2021-07-22 19:15:39 -0400

    cards file

---
+++ use-my-tech-app/src/Cards.js
@@ -0,0 +1,45 @@
+
+import './App.css';
+import React from 'react';
+import { Card, Button, CardTitle, CardText } from 'reactstrap';
+
+function Cards(props) {
+  
+
+  return (
+    <div>
+      <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+        <CardTitle tag="h5">Product 1</CardTitle>
+        <CardText>Product Description.</CardText>
+        <Button>Button</Button>
+      </Card>
+      <Card body inverse color="primary">
+        <CardTitle tag="h5">Product 2</CardTitle>
+        <CardText>Product Description.</CardText>
+        <Button color="secondary">Button</Button>
+      </Card>
+      <Card body inverse color="success">
+        <CardTitle tag="h5">Product 3</CardTitle>
+        <CardText>Product Description.</CardText>
+        <Button color="secondary">Button</Button>
+      </Card>
+      <Card body inverse color="info">
+        <CardTitle tag="h5">Product 4</CardTitle>
+        <CardText>Product Description.</CardText>
+        <Button color="secondary">Button</Button>
+      </Card>
+      <Card body inverse color="warning">
+        <CardTitle tag="h5">Product 5</CardTitle>
+        <CardText>Product Description.</CardText>
+        <Button color="secondary">Button</Button>
+      </Card>
+      <Card body inverse color="danger">
+        <CardTitle tag="h5">Product 6</CardTitle>
+        <CardText>Product Description.</CardText>
+        <Button color="secondary">Button</Button>
+      </Card>
+    </div>
+  );
+}
+
+export default Cards;
