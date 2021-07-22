1f662293fcdd5086447670930ee435319272c76e
king-handy <a.king.life@pm.me>
2021-07-22 09:08:56 -0400

    updated descriptions

--- use-my-tech-app/src/App.js
+++ use-my-tech-app/src/App.js
@@ -9,33 +9,33 @@ function App(props) {
   return (
     <div>
       <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-        <CardTitle tag="h5">Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <CardTitle tag="h5">Product 1</CardTitle>
+        <CardText>Product Description.</CardText>
         <Button>Button</Button>
       </Card>
       <Card body inverse color="primary">
-        <CardTitle tag="h5">Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <CardTitle tag="h5">Product 2</CardTitle>
+        <CardText>Product Description.</CardText>
         <Button color="secondary">Button</Button>
       </Card>
       <Card body inverse color="success">
-        <CardTitle tag="h5">Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <CardTitle tag="h5">Product 3</CardTitle>
+        <CardText>Product Description.</CardText>
         <Button color="secondary">Button</Button>
       </Card>
       <Card body inverse color="info">
-        <CardTitle tag="h5">Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <CardTitle tag="h5">Product 4</CardTitle>
+        <CardText>Product Description.</CardText>
         <Button color="secondary">Button</Button>
       </Card>
       <Card body inverse color="warning">
-        <CardTitle tag="h5">Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <CardTitle tag="h5">Product 5</CardTitle>
+        <CardText>Product Description.</CardText>
         <Button color="secondary">Button</Button>
       </Card>
       <Card body inverse color="danger">
-        <CardTitle tag="h5">Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <CardTitle tag="h5">Product 6</CardTitle>
+        <CardText>Product Description.</CardText>
         <Button color="secondary">Button</Button>
       </Card>
     </div>
